a7b269c31c3a945b5ab54f549ae749de07293b84
kagoston <79797220+kagoston@users.noreply.github.com>
2022-10-09 19:12:54 +0200

    Add files via upload
    portfolio django project

---
+++ personal_portfolio/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def index(request):
+    context = {}
+    return render(request, 'index.html', context)
+
+def about(request):
+    context = {}
+    return render(request, 'about.html', context)
